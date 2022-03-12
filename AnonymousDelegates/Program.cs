aac4a016e2e6474cf1f279d125b149d000881cf1
muratilgun <muratilgun34@gmail.com>
2022-03-12 21:15:29 +0300

    AnonymousDelegates

---
+++ AnonymousDelegates/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AnonymousDelegates
+{
+    class Program
+    {
+        public delegate string MyDelegate(int arg1, int arg2);
+        static void Main(string[] args)
+        {
+            MyDelegate f = delegate(int arg1, int arg2)
+            {
+                return (arg1 + arg2).ToString();
+            };
+            Console.WriteLine("The result is " +f(10,20));
+        }
+    }
+}
