dd8fa36218a766555b7ea95ad484c305fdba59cc
muratilgun <muratilgun34@gmail.com>
2022-03-16 21:06:08 +0300

    LambdaDelegates

---
+++ LambdaDelegates/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace LambdaDelegates
+{
+    public delegate void myEventHandler(string value);
+
+    class MyClass
+    {
+        private string theVal;
+        public event myEventHandler valueChanged;
+
+        public string Val
+        {
+            set
+            {
+                this.theVal = value;
+                this.valueChanged(theVal);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj = new MyClass();
+
+            obj.valueChanged += (x) =>
+            {
+                Console.WriteLine("The value changed to {0}", x);
+            };
+
+            string str;
+            do
+            {
+                str = Console.ReadLine();
+                if (!str.Equals("exit"))
+                {
+                    obj.Val = str;
+                }
+            } while (!str.Equals("exit"));
+
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
