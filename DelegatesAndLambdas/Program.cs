0145ea1415a845c9c128f15cc55e5d3b6835e365
muratilgun <muratilgun34@gmail.com>
2022-03-17 21:37:12 +0300

    DelegatesAndLambdas

---
+++ DelegatesAndLambdas/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DelegatesAndLambdas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOp f = Add;
+            f = Subtract;
+            Console.WriteLine(f(8,2));
+        }
+
+        delegate int MathOp(int x, int y);
+
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        
+    }
+}
