66bcfabd72cc784d3c4b568c5e3199deec217e96
muratilgun <muratilgun34@gmail.com>
2022-03-17 22:10:29 +0300

    Lambdas

--- DelegatesAndLambdas/Program.cs
+++ DelegatesAndLambdas/Program.cs
@@ -8,7 +8,15 @@ namespace DelegatesAndLambdas
         {
             MathOp f = Add;
             f = Subtract;
-            Console.WriteLine(f(8,2));
+            // Console.WriteLine(f(8,2));
+            // CalculateAndPrint(2,1, f);
+            // CalculateAndPrint(21,21,  int Add(int x, int y) { return x + y; });
+            // CalculateAndPrint(21,21, delegate(int x, int y) { return x + y; });
+            // CalculateAndPrint(21,21, (int x, int y)=> { return x + y; });
+            // CalculateAndPrint(21,21, (int x, int y) => x + y );
+            CalculateAndPrint(21,21, ( x, y) => x + y );
+            CalculateAndPrint(21,21, ( x, y) => x - y );
+            CalculateAndPrint(21,21, ( x, y) => x * y );
         }
 
         delegate int MathOp(int x, int y);
@@ -22,6 +30,11 @@ namespace DelegatesAndLambdas
         {
             return a - b;
         }
-        
+
+        static void CalculateAndPrint(int x, int y, MathOp f)
+        {
+            var result = f(x, y);
+            Console.WriteLine(result);
+        }
     }
 }
