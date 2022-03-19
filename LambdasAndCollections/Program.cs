0aa16109b3fb9b338101d9c3d437651094574daa
muratilgun <muratilgun34@gmail.com>
2022-03-19 20:03:29 +0300

    .net 5 records

---
+++ LambdasAndCollections/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+var heroes = new List<Hero>
+{
+    new("Wade","Wilson","Deadpool",false),
+    new(string.Empty,string.Empty,"Homelander",true),
+    new("Bruce","Wayne","Batman",false),
+    new(string.Empty,string.Empty,"Stormfront",true)
+};
+
+var result = FilterHeroesWhoCanFly(heroes);
+var heroesWhoCanFly = string.Join(", ", result);
+Console.WriteLine(heroesWhoCanFly);
+
+List<Hero> FilterHeroesWhoCanFly(List<Hero> heroes)
+{
+    var resultList = new List<Hero>();
+    foreach (var hero in heroes)
+    {
+        if (hero.CanFly)
+        {
+            resultList.Add(hero);
+        }
+    }
+
+    return resultList;
+}
+record Hero(string FirstName, string LastName, string HeroName, bool CanFly);
